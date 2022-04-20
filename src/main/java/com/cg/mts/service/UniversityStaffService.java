e6591b419741970d0bed18587af13a63fdf6b7ba
Ayushi <87642128+ayushikeshri@users.noreply.github.com>
2022-04-20 16:05:11 +0530

    Update UniversityStaffService.java

--- src/main/java/com/cg/mts/service/UniversityStaffService.java
+++ src/main/java/com/cg/mts/service/UniversityStaffService.java
@@ -64,6 +64,52 @@ public class UniversityStaffService implements IUniversityStaffService {
 
 	
 	}
+ abstract class UniversityStaffService1  implements ICourseService{
+		
+		@Autowired(required=true)
+		CourseRepository repository;
+
+		//addCourse()
+		public Course addCourse(Course c) {
+			return repository.save(c);
+		}
+
+		//removeCourse()
+		public String removeCourse(int id) {
+			Course c1=repository.findById(id).get();
+			if(c1==null) { 
+				return "No record present with given id"; 
+			} 
+			else { 
+				repository.delete(c1); 
+				return"Course deleted sucessfully";
+			}
+		}
+
+		//updateCourse()
+		public Course updateCourse(int id, Course c) {
+			Course c1=repository.findById(id).get(); 
+			
+			if(c1!=null) { 
+				c1.setCourseId(c.getCourseId()); 
+				c1.setCourseName(c.getCourseName());
+				c1.setCourseDuration(c.getCourseDuration());
+				c1.setCourseStartDate(c.getCourseStartDate());
+				c1.setCourseEndDate(c.getCourseEndDate());
+				c1.setCourseFees(c.getCourseFees());
+				
+				return repository.save(c1); 
+			} 
+			else 
+				return c1;
+		}
+
+
+
+	
+	}
+
+	 
 
 
 	
